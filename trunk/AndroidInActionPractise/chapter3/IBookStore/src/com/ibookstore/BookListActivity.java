0e2cbe5619fcbf1b8375cc459841368e4a60af41
eric.wong.happy@gmail.com <eric.wong.happy@gmail.com@832e326f-9ca2-a786-5848-70072a7b63aa>
2012-04-23 09:28:07 +0000

    测试增加第三方JAR包的功能
    增加条件验证的功能
    
    测试通过Application传值

--- trunk/AndroidInActionPractise/chapter3/IBookStore/src/com/ibookstore/BookListActivity.java
+++ trunk/AndroidInActionPractise/chapter3/IBookStore/src/com/ibookstore/BookListActivity.java
@@ -1,10 +1,15 @@
 package com.ibookstore;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class BookListActivity extends Activity {
-
+public class BookListActivity extends ListActivity {
+	
+	private TextView label_empty;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -12,5 +17,20 @@ public class BookListActivity extends Activity {
 		
 		
 	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		this.setContentView(R.layout.booklist_activity);
+		label_empty =  (TextView)findViewById(R.id.label_empty);
+		
+		 // set list properties
+        final ListView listView = getListView();
+        listView.setItemsCanFocus(false);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listView.setEmptyView(this.label_empty);
+        
+	}
+	
 	
 }
